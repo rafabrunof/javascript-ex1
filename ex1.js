623a954fdb934dd9d5588dfc5eca2deb36dcd63c
rafabrunof <44882072+rafabrunof@users.noreply.github.com>
2018-12-29 16:39:09 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,32 @@
+console.log("Questão 1");
+console.log("O arquivo 'ex1.js' está funcionando!");
+
+console.log('-----------------------------------------------------------------');
+
+console.log("Questão 2");
+var nome = 'Rafael Bruno';
+var ano_nasc = 1998;
+var ano_atual = 2018;
+var idade = ano_atual - ano_nasc;
+console.log("Olá, eu me chamo " + nome + ", tenho " + idade + " anos e estou estudando JavaScript.");
+
+console.log('-----------------------------------------------------------------');
+console.log("Questão 3");
+var nome = 'Rafael';
+var matricula = 201708338586;
+var nota1 = 8;
+var nota2 = 9;
+media = (nota1 + nota2) / 2;
+console.log("O aluno " + nome + ", matrícula " + matricula + " obteve a média final: " + media);
+
+console.log('-----------------------------------------------------------------');
+console.log("Questão 4");
+var telefone = "984316990";
+console.log("Resultado do teste: " + (telefone.length == 9));
+
+console.log('-----------------------------------------------------------------');
+console.log("Questão 5");
+console.log(Math.pow(32, 6));
+
+
+
